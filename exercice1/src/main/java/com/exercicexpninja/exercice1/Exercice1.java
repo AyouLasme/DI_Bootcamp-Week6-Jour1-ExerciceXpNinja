9973ec20aa248cb7d16d7dec72840f5fe370d482
Ayou Lasme <ayou.lasme@mail.com>
2023-02-04 22:13:59 +0000

    Exercice Xp Ninja Week6 Jour1

---
+++ exercice1/src/main/java/com/exercicexpninja/exercice1/Exercice1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.exercicexpninja.exercice1;
+
+
+public class Exercice1 { 
+    
+    public static void main(String[] args) {
+        
+        DataType info = new DataType(25, 70.0, false, 'M', "Konate Mamadou K");
+        
+        info.displayInfo();
+      
+   }
+}
